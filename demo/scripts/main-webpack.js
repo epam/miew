b6c6878a5315fd93b98cc62a8fd1eeebfff9a160
Aleksei Terentev <Aleksei_Terentev@epam.com>
2017-09-15 10:15:08 +0300

    Merge app main into main-webpack

--- demo/scripts/main-webpack.js
+++ demo/scripts/main-webpack.js
@@ -1,17 +1,134 @@
-require('../../node_modules/bootstrap/dist/css/bootstrap.css');
-require('../../node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css');
-require('../../node_modules/toastr/build/toastr.min.css');
-require('../../node_modules/jquery.terminal/css/jquery.terminal.min.css');
-require('../styles/main.scss');
-require('es6-promise');
+/* global COOKIE_PATH:false */
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css';
+import '../../node_modules/toastr/build/toastr.min.css';
+import '../../node_modules/jquery.terminal/css/jquery.terminal.min.css';
+import '../styles/main.scss';
+import 'es6-promise';
+
+import Miew from './Miew.full';
+import Menu from './ui/Menu';
+import $ from 'jquery';
+import toastr from 'toastr';
 
 window.DEBUG = true;
 
-var div = document.createElement('div');
+const div = document.createElement('div');
 div.className = 'miew-container';
 div.innerHTML = '<div class="miew-message"><p>Please wait<br><small>while viewer is loading</small></p></div>';
 
-var body = document.querySelector('body');
+const body = document.querySelector('body');
 body.appendChild(div);
 
-require('./main');
+function onError(err) {
+  var doc = document.createDocumentFragment();
+
+  var containers = document.getElementsByClassName('miew-container');
+  var parent = containers.length > 0 ? containers[0] : null;
+  var element = document.getElementById('miew-error');
+
+  // on the first error
+  if (!element) {
+    // clear the container
+    while (parent && parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+
+    // create message box
+    element = document.createElement('div');
+    element.setAttribute('class', 'miew-message');
+    doc.appendChild(element);
+
+    var par = element.appendChild(document.createElement('p'));
+    par.appendChild(document.createTextNode('We are sorry'));
+    par.appendChild(document.createElement('br'));
+    par.appendChild(document.createElement('small')).textContent = 'for the failure';
+
+    element = document.createElement('div');
+    element.setAttribute('class', 'miew-error');
+    element.id = 'miew-error';
+    doc.appendChild(element);
+  }
+
+  // append the error details
+  var child = element.appendChild(document.createElement('p'));
+  child.appendChild(document.createTextNode('Error details:'));
+  child = element.appendChild(document.createElement('pre'));
+  if (!err.stack || String(err.stack).indexOf(String(err)) === -1) {
+    child.appendChild(document.createTextNode(err + '\n'));
+  }
+  if (err.stack) {
+    child.appendChild(document.createTextNode(err.stack + '\n'));
+  }
+
+  if (parent) {
+    parent.appendChild(doc);
+  }
+}
+
+// requirejs.onError = onError;
+
+window.onerror = function(err, url, line, col, obj) {
+  onError(obj = obj || {
+    name: 'window.onerror',
+    message: err,
+    sourceURL: url,
+    line: line,
+    column: col
+  });
+  throw obj;
+};
+
+// Uncomment this to profile parsing
+// Miew.profile('data/4TNW.pdb', 10, $('miew-container').first.firstChild.firstChild);
+
+var iframe = window.parent && window.parent.document.getElementById('miew-iframe');
+
+// allow to access viewer objects from console
+if (DEBUG) {
+  window.MIEWS = [];
+}
+
+// create viewer (and run it) for each container element on the page
+$('.miew-container').each(function(i, container) {
+
+  var viewer = new Miew($.extend(
+    true,
+    {
+      container: container,
+      iframe: iframe,
+      load: 'data/1CRN.pdb',
+      cookiePath: typeof COOKIE_PATH !== 'undefined' && COOKIE_PATH || '/',
+    },
+    Miew.options.fromAttr(container.getAttribute('data-miew')),
+    Miew.options.fromURL(window.location.search)
+  ));
+
+  var convertLevel = {
+    'error': 'error',
+    'warn': 'warning',
+    'report': 'info',
+  };
+  toastr.options.newestOnTop = false;
+  viewer.logger.addEventListener('message', function onLogMessage(e) {
+    var level = convertLevel[e.level];
+    if (level) {
+      toastr[level](e.message);
+    }
+  });
+
+  if (DEBUG) {
+    window.MIEWS.push(viewer);
+  }
+
+  var menu = new Menu(viewer);
+
+  // postpone startup inside iframe
+  // TODO: Think of a better way to check if we need to initialize immediately
+  if (!viewer._iframe && viewer.init()) {
+    viewer.benchmarkGfx().then(function() {
+      menu.showOverlay();
+      viewer.run();
+    });
+  }
+});
