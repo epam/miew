2c66507c7f3de96d7201d7fb01d538c8dd8bb5ad
Pavel Smirnov <Pavel_Smirnov1@epam.com>
2017-09-29 12:57:37 +0300

    Add functional tests for CCP4 parser

---
+++ test/functional/data/volumeTestData.js
@@ -0,0 +1,16 @@
+export default [{
+  // just small protein loaded by default
+  name: '4RTN',
+  formats: ['ccp4'],
+  subdivisions: {
+    x: 87,
+    y: 79,
+    z: 72,
+  },
+  range: {
+    x: [-28.796, 41.960],
+    y: [-33.502, 35.265],
+    z: [61.735, 122.612],
+    precision: 0.0005,
+  },
+}];
--- test/functional/parsing.test.js
+++ test/functional/parsing.test.js
@@ -1,13 +1,16 @@
 import PDBParser from '../../src/io/parsers/PDBParser';
 import CIFParser from '../../src/io/parsers/CIFParser';
 import MMTFParser from '../../src/io/parsers/MMTFParser';
+import CCP4Parser from '../../src/io/parsers/CCP4Parser';
 
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 import chai, {expect} from 'chai';
 import dirtyChai from 'dirty-chai';
 
 import complexTestData from './data/complexTestData';
+import volumeTestData from './data/volumeTestData';
 
 chai.use(dirtyChai);
 
@@ -32,6 +35,12 @@ const formats = {
     encoding: null,
     Parser: MMTFParser,
   },
+  ccp4: {
+    name: 'CCP4',
+    extension: 'ccp4',
+    encoding: null,
+    Parser: CCP4Parser,
+  },
 };
 
 function parse(entry, format) {
@@ -52,14 +61,30 @@ function parse(entry, format) {
 }
 
 function selectByFormat(value, formatId) {
-  if (value !== null && typeof value === 'object') {
+  if (value !== undefined && value !== null && typeof value === 'object') {
     const specific = value[formatId];
     return specific !== undefined ? specific : value.default;
   }
   return value;
 }
 
-describe('Complex data', () => {
+function createLookForAssertion(dataSet, formatId) {
+  return (prop, value, precision) => {
+    value = selectByFormat(value, formatId);
+    precision = selectByFormat(precision, formatId);
+    if (value !== undefined) {
+      if (precision !== undefined) {
+        expect(dataSet).to.have.nested.property(prop);
+        expect(_.get(dataSet, prop)).to.be.closeTo(value, precision);
+      } else {
+        expect(dataSet).to.have.nested.property(prop, value);
+      }
+    }
+  };
+
+}
+
+describe('Parsed Complex data', () => {
 
   complexTestData.forEach((entry) => {
     describe(`for ${entry.name}`, () => {
@@ -76,12 +101,7 @@ describe('Complex data', () => {
           return parse(entry, format)
             .then((complex) => {
 
-              const lookFor = (prop, value) => {
-                value = selectByFormat(value, formatId);
-                if (value !== undefined) {
-                  expect(complex).to.have.nested.property(prop, value);
-                }
-              };
+              const lookFor = createLookForAssertion(complex, formatId);
 
               lookFor('_atoms.length', entry.num.atoms);
               lookFor('_bonds.length', entry.num.bonds);
@@ -94,6 +114,44 @@ describe('Complex data', () => {
 
               lookFor('_helices.length', entry.num.helices);
               lookFor('_sheets.length', entry.num.sheets);
+            });
+        });
+      });
+
+    });
+  });
+
+});
+
+describe('Parsed Volume data', () => {
+
+  volumeTestData.forEach((entry) => {
+    describe(`for ${entry.name}`, () => {
+
+      entry.formats.forEach((formatId) => {
+        const format = formats[formatId];
+        it(`looks good in ${format.name} format`, function() {
+          this.timeout(0);
+          if (entry.skip && entry.skip[formatId]) {
+            this.skip();
+            return null;
+          }
+
+          return parse(entry, format)
+            .then((volume) => {
+
+              const lookFor = createLookForAssertion(volume, formatId);
+
+              lookFor('_dimX', entry.subdivisions.x);
+              lookFor('_dimY', entry.subdivisions.y);
+              lookFor('_dimZ', entry.subdivisions.z);
+
+              lookFor('_box.min.x', entry.range.x[0], entry.range.precision);
+              lookFor('_box.max.x', entry.range.x[1], entry.range.precision);
+              lookFor('_box.min.y', entry.range.y[0], entry.range.precision);
+              lookFor('_box.max.y', entry.range.y[1], entry.range.precision);
+              lookFor('_box.min.z', entry.range.z[0], entry.range.precision);
+              lookFor('_box.max.z', entry.range.z[1], entry.range.precision);
             });
         });
       });
