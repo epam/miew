096134d7df99cb5822cd3bb0dc250a40a070aee3
Pavel Smirnov <Pavel_Smirnov1@epam.com>
2017-11-20 10:41:12 +0300

    Rewrite Parser base class in ES2015

--- src/io/parsers/Parser.js
+++ src/io/parsers/Parser.js
@@ -1,62 +1,53 @@
-
-
-//////////////////////////////////////////////////////////////////////////////
 import ContextDependent from '../../utils/ContextDependent';
-//////////////////////////////////////////////////////////////////////////////
-function Parser(data, options) {
-  this._data = data;
-  this._options = options;
-}
 
-////////////////////////////////////////////////////////////////////////////
-// Instance methods
-
-ContextDependent(Parser.prototype);
-
-/** @deprecated */
-Parser.prototype.parseOLD = function(callback) {
-  return this.parse()
-    .then((result) => {
-      callback.ready(result);
-    })
-    .catch((error) => {
-      callback.error(error);
-    });
-};
-
-Parser.prototype.parse = function(/** @deprecated */ callbacks) {
-  if (callbacks) {
-    return this.parseOLD(callbacks);
+export default class Parser {
+  constructor(data, options) {
+    this._data = data;
+    this._options = options;
   }
 
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        return resolve(this.parseSync());
-      } catch (error) {
-        return reject(error);
-      }
+  parseSync() {
+    throw new Error('Parsing this type of data is not implemented');
+  }
+
+  parse(/** @deprecated */ callbacks) {
+    if (callbacks) {
+      return this._parseOLD(callbacks);
+    }
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          return resolve(this.parseSync());
+        } catch (error) {
+          return reject(error);
+        }
+      });
     });
-  });
-};
+  }
 
-Parser.prototype.parseSync = function() {
-  throw new Error('Parsing this type of data is not implemented');
-};
+  /** @deprecated */
+  _parseOLD(callbacks) {
+    return this.parse()
+      .then((result) => {
+        callbacks.ready(result);
+      })
+      .catch((error) => {
+        callbacks.error(error);
+      });
+  }
 
-Parser.prototype.abort = function() {
-  this._abort = true;
-};
+  abort() {
+    this._abort = true;
+  }
 
+  static checkDataTypeOptions(options, type, extension) {
+    const fileType = options.fileType;
+    const name = options.fileName;
+    extension = extension || ('.' + type);
+    return (fileType && fileType === type) ||
+      (name && name.toLowerCase().lastIndexOf(extension) === name.length - extension.length);
+  }
+}
 
-Parser.checkDataTypeOptions = function(options, type, extension) {
-  const fileType = options.fileType;
-  const name = options.fileName;
-  extension = extension || ('.' + type);
-  return (fileType && fileType === type) ||
-    (name && name.toLowerCase().lastIndexOf(extension) === name.length - extension.length);
-};
-////////////////////////////////////////////////////////////////////////////
-
-export default Parser;
-
+ContextDependent(Parser.prototype);
