8f00ae5e340629c996cf6c17a9bebfa7529c2329
Ekaterina Priimak <prijmak.ev@edu.spbstu.ru>
2018-07-05 11:42:22 +0300

    Support ES2015 in modes (classes)

--- src/gfx/modes/LabeledMode.js
+++ src/gfx/modes/LabeledMode.js
@@ -4,29 +4,28 @@
 import Mode from './Mode';
 
 /** @deprecated Old-fashioned atom labels, to be removed in the next major version. */
-function LabeledMode(opts) {
-  Mode.call(this, opts);
-  this.depGroups = this.depGroups.slice(0); // clone depGroups to prevent prototype edits
-  this._staticGroups = this.depGroups.length;
-}
-
-LabeledMode.prototype = Object.create(Mode.prototype);
-LabeledMode.prototype.constructor = LabeledMode;
-
-LabeledMode.prototype.update = function() {
-  var statGroups = this._staticGroups;
-  if (this.settings.now.labels === 'no') {
-    this.depGroups = this.depGroups.slice(0, statGroups);
-  } else {
-    this.depGroups[statGroups] = 'TextLabelsGeo';
-    this.depGroups[statGroups + 1] = 'SGroupsLabels';
+class LabeledMode extends Mode {
+  constructor(opts) {
+    super(opts);
+    this.depGroups = this.depGroups.slice(0); // clone depGroups to prevent prototype edits
+    this._staticGroups = this.depGroups.length;
   }
-};
 
-LabeledMode.prototype.buildGeometry = function(complex, colorer, mask, material) {
-  this.update();
-  return  Mode.prototype.buildGeometry.call(this, complex, colorer, mask, material);
-};
+  update() {
+    var statGroups = this._staticGroups;
+    if (this.settings.now.labels === 'no') {
+      this.depGroups = this.depGroups.slice(0, statGroups);
+    } else {
+      this.depGroups[statGroups] = 'TextLabelsGeo';
+      this.depGroups[statGroups + 1] = 'SGroupsLabels';
+    }
+  }
+
+  buildGeometry(complex, colorer, mask, material) {
+    this.update();
+    return Mode.prototype.buildGeometry.call(this, complex, colorer, mask, material);
+  }
+}
 
 export default LabeledMode;
 
