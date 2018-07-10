6967697aed099fcd8bd14a5f2c03ecb64b4614b2
AdayevK <adayevkp@gmail.com>
2018-07-10 14:02:35 +0300

    Add classes and mixins according to ES2015

--- src/gfx/geometries/SimpleSpheresGeometry.js
+++ src/gfx/geometries/SimpleSpheresGeometry.js
@@ -1,6 +1,5 @@
 
 
-import _ from 'lodash';
 import * as THREE from 'three';
 import utils from '../../utils';
 import ChunkedObjectsGeometry from './ChunkedObjectsGeometry';
@@ -8,44 +7,39 @@ import SphereCollisionGeo from './SphereCollisionGeo';
 
 const VEC_SIZE = 3;
 
-function SimpleSpheresGeometry(spheresCount, sphereComplexity) {
-  const sphGeometry = new THREE.SphereBufferGeometry(
-    1, sphereComplexity * 2, sphereComplexity,
-    0, Math.PI * 2, 0, Math.PI
-  );
-  ChunkedObjectsGeometry.call(this, sphGeometry, spheresCount);
-  SphereCollisionGeo.call(this, spheresCount);
+class SimpleSpheresGeometry extends SphereCollisionGeo(ChunkedObjectsGeometry) {
+  constructor(spheresCount, sphereComplexity) {
+    const sphGeometry = new THREE.SphereBufferGeometry(
+      1, sphereComplexity * 2, sphereComplexity,
+      0, Math.PI * 2, 0, Math.PI
+    );
+    super(spheresCount, sphGeometry, spheresCount);
 
+    const normals = this._normals;
+    const geoNormals = sphGeometry.attributes.normal.array;
+    const chunkSize = this._chunkSize;
+    this._chunkPos = this._chunkGeo.attributes.position.array;
+    this._tmpPositions = utils.allocateTyped(Float32Array, chunkSize * VEC_SIZE);
+    for (let i = 0; i < spheresCount; ++i) {
+      normals.set(geoNormals, chunkSize * VEC_SIZE * i);
+    }
+  }
 
-  const normals = this._normals;
-  const geoNormals = sphGeometry.attributes.normal.array;
-  const chunkSize = this._chunkSize;
-  this._chunkPos = this._chunkGeo.attributes.position.array;
-  this._tmpPositions = utils.allocateTyped(Float32Array, chunkSize * VEC_SIZE);
-  for (let i = 0; i < spheresCount; ++i) {
-    normals.set(geoNormals, chunkSize * VEC_SIZE * i);
+  setItem(itemIdx, itemPos, itemRad) {
+    const tmpPos = this._tmpPositions;
+    const chunkSize = this._chunkSize;
+    const geoPos = this._chunkPos;
+
+    for (let i = 0; i < chunkSize; ++i) {
+      const idx = i * 3;
+      tmpPos[idx] = itemPos.x + geoPos[idx] * itemRad;
+      tmpPos[idx + 1] = itemPos.y + geoPos[idx + 1] * itemRad;
+      tmpPos[idx + 2] = itemPos.z + geoPos[idx + 2] * itemRad;
+    }
+
+    this._positions.set(tmpPos, chunkSize * itemIdx * VEC_SIZE);
+    this.setSphere(itemIdx, itemPos, itemRad);
   }
 }
-
-SimpleSpheresGeometry.prototype = Object.create(ChunkedObjectsGeometry.prototype);
-_.mixin(SimpleSpheresGeometry.prototype, SphereCollisionGeo.prototype);
-SimpleSpheresGeometry.prototype.constructor = SimpleSpheresGeometry;
-
-SimpleSpheresGeometry.prototype.setItem = function(itemIdx, itemPos, itemRad) {
-  const tmpPos = this._tmpPositions;
-  const chunkSize = this._chunkSize;
-  const geoPos = this._chunkPos;
-
-  for (let i = 0; i < chunkSize; ++i) {
-    const idx = i * 3;
-    tmpPos[idx] = itemPos.x + geoPos[idx] * itemRad;
-    tmpPos[idx + 1] = itemPos.y + geoPos[idx + 1] * itemRad;
-    tmpPos[idx + 2] = itemPos.z + geoPos[idx + 2] * itemRad;
-  }
-
-  this._positions.set(tmpPos, chunkSize * itemIdx * VEC_SIZE);
-  this.setSphere(itemIdx, itemPos, itemRad);
-};
-
 export default SimpleSpheresGeometry;
 
--- src/gfx/geometries/SphereCollisionGeo.js
+++ src/gfx/geometries/SphereCollisionGeo.js
@@ -28,62 +28,62 @@ class CollisionSphere {
   }
 }
 
-//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////z
 
-function SphereCollisionGeo(count) {
-  this._objects = new Array(count);
-  this.boundingSphere = null;
-  this.boundingBox = null;
-}
+const SphereCollisionGeo = (base) => class extends base {
+  constructor(count, ...args) {
+    super(...args);
+    this._objects = new Array(count);
+    this.boundingSphere = null;
+    this.boundingBox = null;
+  }
 
-SphereCollisionGeo.constructor = SphereCollisionGeo;
+  setSphere(idx, position, radius) {
+    this._objects[idx] = new CollisionSphere(position, radius);
+  }
 
-SphereCollisionGeo.prototype.setSphere = function(idx, position, radius) {
-  this._objects[idx] = new CollisionSphere(position, radius);
-};
-
-SphereCollisionGeo.prototype.raycast = function(raycaster, intersects) {
-  // TODO raycast with bounding sphere? How to deal with updates?
-  for (var i = 0, n = this._objects.length; i < n; ++i) {
-    var inters = this._objects[i].raycast(raycaster);
-    if (inters) {
-      inters.chunkIdx = i;
-      intersects.push(inters);
+  raycast(raycaster, intersects) {
+    // TODO raycast with bounding sphere? How to deal with updates?
+    for (let i = 0, n = this._objects.length; i < n; ++i) {
+      const inters = this._objects[i].raycast(raycaster);
+      if (inters) {
+        inters.chunkIdx = i;
+        intersects.push(inters);
+      }
     }
   }
-};
 
-SphereCollisionGeo.prototype.computeBoundingBox = function() {
-  var objects = this._objects;
-  var boundingBox = this.boundingBox;
-  if (boundingBox === null) {
-    this.boundingBox = boundingBox = new THREE.Box3();
-  }
-  boundingBox.makeEmpty();
-  for (var i = 0, n = objects.length; i < n; ++i) {
-    boundingBox.expandByPoint(objects[i]._position);
-  }
-};
-
-SphereCollisionGeo.prototype.computeBoundingSphere = function() {
-  this.computeBoundingBox();
-  var objects = this._objects;
-  var boundingBox = this.boundingBox;
-  // Build bounding sphere
-  var radiusSquared = 0.0;
-  var center = new THREE.Vector3();
-  boundingBox.getCenter(center);
-  for (var i = 0, n = objects.length; i < n; ++i) {
-    var pos = objects[i]._position;
-    var lengthSquared = center.distanceToSquared(pos);
-    if (radiusSquared < lengthSquared) {
-      radiusSquared = lengthSquared;
+  computeBoundingBox() {
+    const objects = this._objects;
+    let boundingBox = this.boundingBox;
+    if (boundingBox === null) {
+      this.boundingBox = boundingBox = new THREE.Box3();
+    }
+    boundingBox.makeEmpty();
+    for (let i = 0, n = objects.length; i < n; ++i) {
+      boundingBox.expandByPoint(objects[i]._position);
     }
   }
-  if (this.boundingSphere === null) {
-    this.boundingSphere = new THREE.Sphere();
-  }
-  this.boundingSphere.set(center, Math.sqrt(radiusSquared));
-};
 
+  computeBoundingSphere() {
+    this.computeBoundingBox();
+    const objects = this._objects;
+    const boundingBox = this.boundingBox;
+    // Build bounding sphere
+    let radiusSquared = 0.0;
+    const center = new THREE.Vector3();
+    boundingBox.getCenter(center);
+    for (let i = 0, n = objects.length; i < n; ++i) {
+      const pos = objects[i]._position;
+      const lengthSquared = center.distanceToSquared(pos);
+      if (radiusSquared < lengthSquared) {
+        radiusSquared = lengthSquared;
+      }
+    }
+    if (this.boundingSphere === null) {
+      this.boundingSphere = new THREE.Sphere();
+    }
+    this.boundingSphere.set(center, Math.sqrt(radiusSquared));
+  }
+};
 export default SphereCollisionGeo;
