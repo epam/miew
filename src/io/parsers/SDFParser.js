bed3f89d8cbe9b03d541622bfd5925b8b2f2e4f3
Kirill.Adaev <adayevkp@gmail.com>
2018-10-24 15:03:02 +0300

    Add changes to SDFPsrser and SDFStream

--- src/io/parsers/SDFParser.js
+++ src/io/parsers/SDFParser.js
@@ -15,6 +15,16 @@ const
   Molecule = chem.Molecule;
 
 const chargeMap = [0, 3, 2, 1, 0, -1, -2, -3];
+const bondsMap = [
+  Bond.BondType.UNKNOWN,
+  Bond.BondType.COVALENT,
+  Bond.BondType.COVALENT,
+  Bond.BondType.COVALENT,
+  Bond.BondType.AROMATIC
+];
+
+const sdfAndMolRegexp = /.*(M\s\sEND).*|.*(^$$$$).*|.*>\s+<(.+)>.*/;
+const sdfRegExp = /.*($$$$).*|.*>\s+<(.+)>.*/;
 
 export default class SDFParser extends Parser {
   constructor(data, options) {
@@ -22,23 +32,83 @@ export default class SDFParser extends Parser {
     this._complex = null;
     this._chain = null;
     this._residue = null;
-    this._moleculeIndx = 0;
+    this._molecules = null;
+    this._metadata = {};
+    this._metadata.molecules = [];
+    this._currentMolProps = [];
+    this._compoundIndx = -1;
     this._assemblies = [];
-    //dat kolxoz
     this._atomsParsed = 0;
-    //
   }
 
   canProbablyParse(data) {
-    return _.isString(data);
+    const res = sdfAndMolRegexp.test(data);
+    return _.isString(data) && res;
   }
 
-  _parseConnectionTable() {
-
+  _parseHeader(stream) {
+    const molecule = {};
+    molecule.name = stream.getStringFromStart(0);
+    if (molecule.name.match(/^\d+$/)) {
+      molecule.name = 'CID: ' + molecule.name;
+    }
+    molecule.date = parseInt(stream.getStringFromStart(1).substr(10, 6).trim(), 10) || '';
+    molecule.title = stream.getStringFromStart(2);
+    this._metadata.molecules.push(molecule);
   }
 
-  _parseHeader() {
+  _parseAtoms(stream, atomsNum) {
+    let curStr;
+    let globalSerial;
+    let localSerial = 0;
 
+    for (let i = 0; i < atomsNum; i++) {
+      curStr = stream.getNextString();
+      localSerial++;
+      globalSerial = this._atomsParsed + localSerial;
+      const occupancy = 1.0;
+      const x = parseFloat(curStr.substr(0, 10));
+      const y = parseFloat(curStr.substr(10, 10));
+      const z = parseFloat(curStr.substr(20, 10));
+      const charge = chargeMap[parseInt(curStr.substr(36, 3), 10)];
+      let xyz = new THREE.Vector3(x, y, z);
+      let name = curStr.substr(31, 3).trim().toUpperCase();
+      const type = Element.getByName(name);
+      name += 'A' + globalSerial;
+      const role = Element.Role[name];
+
+      const chainID = 'C' + this._compoundIndx;
+      const resName = 'R' + this._compoundIndx;
+      const resSeq = resName;
+      let chain = this._chain;
+      if (!chain || chain.getName() !== chainID) {
+        this._chain = chain = this._complex.getChain(chainID) || this._complex.addChain(chainID);
+        this._residue = null;
+      }
+
+      let residue = this._residue;
+      if (!residue || residue.getSequence() !== resSeq) {
+        this._residue = residue = chain.addResidue(resName, resSeq, '');
+      }
+
+      residue.addAtom(name, type, xyz, role, false, globalSerial, '', occupancy, 0.0, charge);
+    }
+  }
+
+  _parseBonds(stream, bondsNum) {
+    let curStr;
+
+    for (let i = 0; i < bondsNum; i++) {
+      curStr = stream.getNextString();
+      let atom1 = parseInt(curStr.substr(0, 3), 10) + this._atomsParsed;
+      let atom2 = parseInt(curStr.substr(3, 3), 10) + this._atomsParsed;
+      const bondType = parseInt(curStr.substr(6, 3), 10);
+
+      if (atom1 > atom2) {
+        [atom1, atom2] = [atom2, atom1];
+      }
+      this._complex.addBond(atom1, atom2, 0, bondsMap[bondType], true);
+    }
   }
 
   _fixBondsArray() {
@@ -65,36 +135,63 @@ export default class SDFParser extends Parser {
     }
   }
 
-  buildAssemblies() {
+  _buildAssemblies() {
     const chains = this._complex._chains;
-    const assemblies = [];
 
     for (let i = 0; i < chains.length; i++) {
       const assembly = new Assembly(this._complex);
-      assembly.addChain(chains[i]._name)
-      assemblies.push(assembly);
+      let matrix = new THREE.Matrix4();
+      assembly.addMatrix(matrix);
+      assembly.addChain(chains[i]._name);
+      this._assemblies.push(assembly);
     }
+  }
 
-    return assemblies;
+  _buildMolecules() {
+    this._complex._molecules = [];
+    const molecules = this._metadata.molecules;
+    for (let i = 0; i < molecules.length; i++) {
+      const molecule = new Molecule(this._complex, molecules[i].name, i + 1);
+      molecule._residues = molecules[i]._residues;
+      this._complex._molecules[i] = molecule;
+    }
+  }
+
+  _finalizeMetadata() {
+    const molecules = this._metadata.molecules;
+    const metadata = this._complex.metadata;
+    const complex = this._complex;
+    if (molecules.length === 1) {
+      complex.name = molecules[0].name;
+      metadata.title = molecules[0].title;
+      metadata.date = molecules[0].date;
+      metadata.properties = molecules[0].props;
+    } else {
+      metadata.molecules = molecules;
+      complex.name = 'yolo';
+    }
   }
 
   finalize() {
     const serialAtomMap = this._serialAtomMap = {};
     const atoms = this._complex._atoms;
+
     let i = 0, ni = atoms.length;
     for (; i < ni; ++i) {
-      var atom = atoms[i];
+      const atom = atoms[i];
       serialAtomMap[atom._serial] = atom;
     }
 
     this._complex._finalizeBonds();
     this._fixBondsArray();
-    this._assemblies = this.buildAssemblies();
+    this._buildAssemblies();
+    this._buildMolecules();
+    this._finalizeMetadata();
 
-    //this._complex.units = this._complex.units.concat(this._assemblies);
+    this._complex.units = this._complex.units.concat(this._assemblies);
 
     this._complex.finalize({
-      needAutoBonding: true,
+      needAutoBonding: false,
       detectAromaticLoops: this.settings.now.aromatic,
       enableEditing: this.settings.now.editing,
       serialAtomMap: this._serialAtomMap
@@ -102,102 +199,70 @@ export default class SDFParser extends Parser {
   }
 
   _parseMOL(stream) {
-    this._moleculeIndx++;
-    let parsedStrings = 0;
-    let curStr;
+    this._compoundIndx++;
 
-    let serial = 0;
-    //kolxoz
-    let trueSerial;
-    //
+    this._parseHeader(stream);
     const countsLine = stream.getStringFromStart(3);
     const atomsNum = parseInt(countsLine.substr(0, 3), 10);
     const bondsNum = parseInt(countsLine.substr(3, 3), 10);
+    this._parseAtoms(stream, atomsNum);
+    this._parseBonds(stream, bondsNum);
 
-    parsedStrings += 4;
-
-    const complex = this._complex;
-
-    for (let i = 0; i < atomsNum; i++) {
+    let curStr;
+    do {
       curStr = stream.getNextString();
-      serial++;
-      trueSerial = this._atomsParsed + serial;
-      const occupancy = 1.0;
-      const x = parseFloat(curStr.substr(0, 10));
-      const y = parseFloat(curStr.substr(10, 10));
-      const z = parseFloat(curStr.substr(20, 10));
-      const charge = chargeMap[parseInt(curStr.substr(36, 3), 10)];
-      let xyz = new THREE.Vector3(x, y, z);
-      let name = curStr.substr(31, 3).trim().toUpperCase();
-      const type = Element.getByName(name);
-      name += trueSerial;
-      const role = Element.Role[name];
+    } while (curStr !== 'M  END');
 
-      // NOTE: Residues of a particular chain are not required to be listed next to each other.
-      // https://github.com/biasmv/pv/commit/7319b898b7473ba380c26699e3b028b2b1a7e1a1
+    this._atomsParsed += atomsNum;
 
-      const chainID = 'chain' + this._moleculeIndx;
-      const resName = 'residue' + this._moleculeIndx;
-      const resSeq = resName;
-      const iCode = '';
-      let chain = this._chain;
-      if (!chain || chain.getName() !== chainID) {
-        this._chain = chain = this._complex.getChain(chainID) || this._complex.addChain(chainID);
-        this._residue = null;
-      }
-
-      var residue = this._residue;
-      if (!residue || residue.getSequence() !== resSeq || residue.getICode() !== iCode) {
-        this._residue = residue = chain.addResidue(resName, resSeq, iCode);
-      }
-
-      residue.addAtom(name, type, xyz, role, false, trueSerial, '', occupancy, 0.0, charge);
-
-      parsedStrings++;
-    }
-
-    for (let i = 0; i < bondsNum; i++) {
-      curStr = stream.getNextString();
-      const atom1 = parseInt(curStr.substr(0, 3), 10) + this._atomsParsed;
-      const atom2 = parseInt(curStr.substr(3, 3), 10) + this._atomsParsed;
-      //const bondType = Bond.BondType.UNKNOWN;//parseInt(curStr.substr(6, 3), 10);
-
-      //dat kolxoz
-      if (atom1 > atom2) {
-        complex.addBond(atom2, atom1, 0, 1, true);
-      } else {
-        complex.addBond(atom1, atom2, 0, 1, true);
-      }
-
-      parsedStrings++;
-    }
-
-    this._atomsParsed += serial;
+    this._metadata.molecules[this._compoundIndx]._residues = [];
+    this._metadata.molecules[this._compoundIndx]._residues.push(this._residue);
   }
 
-  _parseDataItems(stream) {
-    let parsedStrings = 0;
+  _parseDataItem(stream) {
+    const tag = stream.getCurrentString();
+    let data = [];
     let curStr = stream.getNextString();
-    while (curStr !== '$$$$' && !_.isUndefined(curStr)) {
+    while (curStr.trim() !== '') {
+      data.push(curStr);
       curStr = stream.getNextString();
-      parsedStrings++;
     }
-
-    return parsedStrings;
+    if (data.length === 1) {
+      data = data[0];
+    }
+    const property = {tag: tag.replace(/<|>/g, '').trim(), data: data};
+    this._currentMolProps.push(property);
   }
 
   _parseCompound(stream) {
-    let strParsed = 0;
-    strParsed += this._parseMOL(stream);
-    //strParsed += this._parseDataItems(stream);
-    return strParsed;
+    this._parseMOL(stream);
+    while (stream.findNextDataItem()) {
+      this._parseDataItem(stream);
+    }
+
+    const molecule = this._metadata.molecules[this._compoundIndx];
+    if (this._currentMolProps.length !== 0) {
+      molecule.props = this._currentMolProps;
+    }
+  }
+
+  _defineFormat(data) {
+    let format;
+    if (sdfRegExp.test(data)) {
+      format = 'sdf';
+    } else {
+      format = 'mol';
+    }
+
+    return format;
   }
 
   parseSync() {
     const result = this._complex = new Complex();
     const stream = new SDFStream(this._data);
 
-    while (stream.haveStrings()) {
+    result.metadata.format = this._defineFormat(this._data);
+    while (stream.probablyHaveDataToParse()) {
       this._parseCompound(stream);
       stream.findNextCompoundStart();
     }
--- src/io/parsers/SDFStream.js
+++ src/io/parsers/SDFStream.js
@@ -2,21 +2,21 @@ export default class SDFStream {
   constructor(data) {
     this._strings = data.split(/\r?\n|\r/);
     this._currentStart = 0;
-    this._currentString = 0;
+    this._currentStringIndx = 0;
   }
 
   setStart(start) {
     if (start >= this._strings.length) {
       this._currentStart = this._strings.length - 1;
-      this._currentString = this._strings.length - 1;
+      this._currentStringIndx = this._strings.length - 1;
     } else {
       this._currentStart = start;
-      this._currentString = start;
+      this._currentStringIndx = start;
     }
   }
 
   getNextString() {
-    return this._strings[++this._currentString];
+    return this._strings[++this._currentStringIndx];
   }
 
   getStringByIndx(indx) {
@@ -24,26 +24,47 @@ export default class SDFStream {
   }
 
   getCurrentString() {
-    return this._strings[this._currentString];
+    return this._strings[this._currentStringIndx];
   }
 
   getStringFromStart(numb) {
-    this._currentString += numb;
+    this._currentStringIndx = this._currentStart + numb;
     return this._strings[this._currentStart + numb];
   }
 
-  findNextCompoundStart() {
+  findNextDataItem() {
     let curStr = this.getNextString();
+    let res = false;
+    while (!_.isUndefined(curStr) && !curStr.match(/>\s+<(.*)>/) && curStr.trim() !== '$$$$') {
+      curStr = this.getNextString();
+    }
+
+    if (this.probablyHaveDataToParse() && curStr.trim() !== '$$$$') {
+      res = true;
+    }
+
+    return res;
+  }
+
+  findNextCompoundStart() {
+    let curStr = this.getCurrentString()
+    let res = false;
     while (curStr !== '$$$$' && !_.isUndefined(curStr)) {
       this._currentStart++;
       curStr = this.getNextString();
     }
-    this._currentString++;
-    this.setStart(this._currentString);
+    this._currentStringIndx++;
+    this.setStart(this._currentStringIndx);
+
+    if (this.probablyHaveDataToParse()) {
+      res = true;
+    }
+
+    return res;
   }
 
-  haveStrings() {
-    if (this._currentString >= this._strings.length - 4) {
+  probablyHaveDataToParse() {
+    if (this._currentStringIndx >= this._strings.length - 2) {
       return false;
     } else {
       return true;
