468b8fee87ad260c2445e82372206ae26c2f1f3d
Nadezda Katsman <katsman96@yandex.ru>
2018-11-13 18:18:26 +0300

    Reorganize code

--- src/io/parsers/XYZParser.js
+++ src/io/parsers/XYZParser.js
@@ -5,30 +5,13 @@ import _ from 'lodash';
 
 const
   Complex = chem.Complex,
-  Element = chem.Element,
-  Molecule = chem.Molecule;
+  Element = chem.Element;
 
 class XYZParser extends Parser {
   constructor(data, options) {
     super(data, options);
 
     this._complex = null;
-    this._chain = null;
-    this._residue = null;
-    this._sheet = null;
-    this._serialAtomMap = null;
-    this._modelId = 1;
-    this._compaundFound = false;
-    this._biomoleculeFound = false;
-    this._allowedChainsIDs = null;
-    this._lastMolId = -1;
-
-    this._remarks = {};
-    this._remark = null;
-
-    this._molecules = [];
-    this._molecule = null;
-    this._compndCurrToken = '';
 
     this._options.fileType = 'xyz';
   }
@@ -44,107 +27,56 @@ class XYZParser extends Parser {
     return _.isString(data) && /^\s*data_/i.test(data);
   }
 
-  _parseToObject(source) {
-    let lines = source.split(/\s*\n\s*/);
+  _parse(source) {
+    const het = true;
+    const altLoc = ' ';
+    const occupancy = 1;
+    const tempFactor = 1;
+    const charge = 0;
 
-    let nAtoms = parseInt(lines[0], 10);
-    let comment = lines[1];
+    const endAtomsNum = source.indexOf('\n');
+    const nAtoms = parseInt(source.substring(0, endAtomsNum), 10);
+    const endComment = source.indexOf('\n', endAtomsNum + 1);
+    const comment = source.slice(endAtomsNum + 1, endComment);
 
-    const metadata = this._complex.metadata;
-    metadata.date = ' ';
+    let lines = source.substring(endComment + 1).split(/[\s,]*\n[\s,]*/);
+    if (!Number.isNaN(nAtoms) && (lines.length - 1 !== nAtoms)) {
+      this._complex.error = {
+        message: 'wrong number of atoms'
+      };
+      return;
+    }
 
+    this._complex.metadata.format = 'xyz';
     this._complex.name = comment;
-    metadata.format = 'xyz';
 
-    this._molecule = {_index: '', _chains: []};
-    this._molecule._index = 0;
-    this._molecule._name = 'UNKNOWN';
-    this._molecules.push(this._molecule);
+    const chain = this._complex.addChain(' ');
+    const residue = chain.addResidue('LIG', 1, ' ');
 
-    this._molecule._chains = this._molecule._chains.concat('U');
-    this._chain = this._complex.addChain('U');
-
-    let residue = this._residue;
-    this._residue = residue = this._chain.addResidue('unknown', 0, ' ');
-
-    for (let i = 0; i < nAtoms; i++) {
-      const words = lines[i + 2].split(/[\s,]+/);
+    for (let i = 0; i < lines.length - 1; i++) {
+      const words = lines[i].split(/[\s,]+/);
 
       if (words.length !== 4) {
         this._complex.error = {
-          line: i + 2,
           message: 'missed parameters'
         };
         break;
       }
 
+      const serial = i + 1;
       const name = words[0];
       const xyz = new THREE.Vector3(parseFloat(words[1]), parseFloat(words[2]), parseFloat(words[3]));
       const type = Element.getByName(name);
       const role = Element.Role[name]; // FIXME: Maybe should use type as additional index (" CA " vs. "CA  ")
 
-      residue.addAtom(name, type, xyz, role, true, i+1, ' ', 1, 1, 0);
-    }
-  }
-
-  _fixChains() {
-    const idChainMap = {};
-    const complex = this._complex;
-
-    //prepare
-    for (let i = 0; i < complex._chains.length; i++) {
-      const chain = complex._chains[i];
-      idChainMap[chain._name.charCodeAt(0)] = chain;
-    }
-  }
-
-  _fixBondsArray() {
-    const serialAtomMap = this._serialAtomMap = {};
-    const complex = this._complex;
-
-    const atoms = complex._atoms;
-    let i = 0, ni = atoms.length;
-    for (; i < ni; ++i) {
-      const atom = atoms[i];
-      serialAtomMap[atom._serial] = atom;
-    }
-  }
-
-  _finalizeMolecules() {
-    // get chains from complex
-    const chainDict = {};
-
-    const chains = this._complex._chains;
-    for (let i = 0; i < chains.length; ++i) {
-      const chainObj = chains[i];
-      const chainName  = chainObj._name;
-      chainDict[chainName] = chainObj;
-    }
-
-    // aggregate residues from chains
-    for (let i = 0; i < this._molecules.length; i++) {
-      const m = this._molecules[i];
-      let residues = [];
-      for (let j = 0; j < m._chains.length; j++) {
-        const name = m._chains[j];
-        const chain = chainDict[name];
-        residues = residues.concat(chain._residues.slice());
-      }
-      const molecule = new Molecule(this._complex, m._name, i + 1);
-      molecule._residues = residues;
-      this._complex._molecules[i] = molecule;
+      residue.addAtom(name, type, xyz, role, het, serial, altLoc, occupancy, tempFactor, charge);
     }
   }
 
   parseSync() {
     const result = this._complex = new Complex();
 
-    this._parseToObject(this._data);
-
-    this._fixBondsArray();
-    this._fixChains();
-
-    this._finalizeMolecules();
+    this._parse(this._data);
 
     this._complex.finalize({
       needAutoBonding: true,
@@ -153,10 +85,6 @@ class XYZParser extends Parser {
       serialAtomMap: this._serialAtomMap
     });
 
-    this._serialAtomMap = null;
-    this._sheet = null;
-    this._residue = null;
-    this._chain = null;
     this._complex = null;
 
     if (result.error) {
