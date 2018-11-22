6be595c60703a99b3cc960ac40ca7aec8b975293
Hadegda <katsman96@yandex.ru>
2018-11-22 13:01:22 +0300

    Support ES2015 in chem (#96)

--- src/chem/AtomPairs.js
+++ src/chem/AtomPairs.js
@@ -1,8 +1,5 @@
-
-
-//////////////////////////////////////////////////////////////////////////////
 import utils from '../utils';
-//////////////////////////////////////////////////////////////////////////////
+
 var cMaxPairsForHashCode = 32;
 var cHashTableSize = 1024 * 1024;
 var cNumbersPerPair = 4;
@@ -11,67 +8,67 @@ var cInvalidVal = -1;
 // 89237 is a large simple number, can be used for pseudo random hash code create
 var cBigPrime = 89237;
 
-function AtomPairs(maxPairsEstimate) {
-  var i = 0;
-
-  this.numPairs = 0;
-  this.numMaxPairs = maxPairsEstimate;
-  this.intBuffer = utils.allocateTyped(Int32Array, maxPairsEstimate * cNumbersPerPair);
-  for (; i < maxPairsEstimate * cNumbersPerPair; i++) {
-    this.intBuffer[i] = cInvalidVal;
+class AtomPairs {
+  constructor(maxPairsEstimate) {
+    this.numPairs = 0;
+    this.numMaxPairs = maxPairsEstimate;
+    this.intBuffer = utils.allocateTyped(Int32Array, maxPairsEstimate * cNumbersPerPair);
+    for (let i = 0; i < maxPairsEstimate * cNumbersPerPair; i++) {
+      this.intBuffer[i] = cInvalidVal;
+    }
+    this.hashBuffer = utils.allocateTyped(Int32Array, cHashTableSize * cMaxPairsForHashCode);
+    for (let i = 0; i < cHashTableSize * cMaxPairsForHashCode; i++) {
+      this.hashBuffer[i] = cInvalidVal;
+    }
   }
-  this.hashBuffer = utils.allocateTyped(Int32Array, cHashTableSize * cMaxPairsForHashCode);
-  for (i = 0; i < cHashTableSize * cMaxPairsForHashCode; i++) {
-    this.hashBuffer[i] = cInvalidVal;
+
+  /**
+   * Destroy all pairs memory
+   */
+  destroy() {
+    this.intBuffer = null;
+    this.hashBuffer = null;
+  }
+
+  /**
+   * Add pair of atoms to collection
+   * @param {number} indexA - Index of the 1st vertex.
+   * @param {number} indexB - Index of the 2nd vertex.
+   */
+  addPair(indexA, indexB) {
+    const ia = (indexA < indexB) ? indexA : indexB;
+    const ib = (indexA > indexB) ? indexA : indexB;
+    const codeToAdd = ia + (ib << cMaxNeighbours);
+
+    const hashCode = (ia + (ib * cBigPrime)) & (cHashTableSize - 1);
+    let j = hashCode * cMaxPairsForHashCode;
+    let apI = 0;
+    for (; apI < cMaxPairsForHashCode; apI++) {
+      const code = this.hashBuffer[j + apI];
+      if (code === cInvalidVal) {
+        break;
+      }
+      if (code === codeToAdd) {
+        return false;
+      }
+    }
+    // add this new hash code
+    if (apI >= cMaxPairsForHashCode) {
+      throw new Error('addPair: increase cMaxPairsForHashCode');
+    }
+    this.hashBuffer[j + apI] = codeToAdd;
+
+    // actually add
+    if (this.numPairs >= this.numMaxPairs) {
+      throw new Error('addPair: increase num pairs');
+    }
+    j = this.numPairs * cNumbersPerPair;
+    this.intBuffer[j] = ia;
+    this.intBuffer[j + 1] = ib;
+    this.intBuffer[j + 2] = codeToAdd;
+    this.numPairs++;
+    return true;
   }
 }
-
-/**
- * Destroy all pairs memory
- */
-AtomPairs.prototype.destroy = function() {
-  this.intBuffer = null;
-  this.hashBuffer = null;
-};
-
-/**
- * Add pair of atoms to collection
- * @param {number} indexA - Index of the 1st vertex.
- * @param {number} indexB - Index of the 2nd vertex.
- */
-AtomPairs.prototype.addPair = function(indexA, indexB) {
-  var ia = (indexA < indexB) ? indexA : indexB;
-  var ib = (indexA > indexB) ? indexA : indexB;
-  var codeToAdd = ia + (ib << cMaxNeighbours);
-
-  var hashCode = (ia + (ib * cBigPrime)) & (cHashTableSize - 1);
-  var j = hashCode * cMaxPairsForHashCode;
-  var apI = 0;
-  for (; apI < cMaxPairsForHashCode; apI++) {
-    var code = this.hashBuffer[j + apI];
-    if (code === cInvalidVal) {
-      break;
-    }
-    if (code === codeToAdd) {
-      return false;
-    }
-  }
-  // add this new hash code
-  if (apI >= cMaxPairsForHashCode) {
-    throw new Error('addPair: increase cMaxPairsForHashCode');
-  }
-  this.hashBuffer[j + apI] = codeToAdd;
-
-  // actually add
-  if (this.numPairs >= this.numMaxPairs) {
-    throw new Error('addPair: increase num pairs');
-  }
-  j = this.numPairs * cNumbersPerPair;
-  this.intBuffer[j] = ia;
-  this.intBuffer[j + 1] = ib;
-  this.intBuffer[j + 2] = codeToAdd;
-  this.numPairs++;
-  return true;
-};
 
 export default AtomPairs;
--- src/chem/BiologicalUnit.js
+++ src/chem/BiologicalUnit.js
@@ -7,75 +7,74 @@ import selectors from './selectors';
  * @exports BiologicalUnit
  * @constructor
  */
-function BiologicalUnit(complex) {
-  this._complex = complex;
-  this._selector = selectors.keyword('All')();
-  this._boundaries = {
-    boundingBox: new THREE.Box3(),
-    boundingSphere: new THREE.Sphere()
-  };
+class BiologicalUnit {
+  constructor(complex) {
+    this._complex = complex;
+    this._selector = selectors.keyword('All')();
+    this._boundaries = {
+      boundingBox: new THREE.Box3(),
+      boundingSphere: new THREE.Sphere()
+    };
+  }
+
+  computeBoundaries() {
+    const atoms = this._complex._atoms;
+    const n = atoms.length;
+    const selector = this._selector;
+
+    const boundingBox = this._boundaries.boundingBox;
+    boundingBox.makeEmpty();
+    if (n === 1) {
+      boundingBox.expandByPoint(atoms[0]._position);
+      const bbc = new THREE.Vector3();
+      boundingBox.getCenter(bbc);
+      const s = 2 * atoms[0].element.radius; //*settings.now.modes.BS.atom; FIXME N: hack commented
+      boundingBox.setFromCenterAndSize(bbc, new THREE.Vector3(s, s, s));
+    } else {
+      for (let i = 0; i < n; ++i) {
+        if (selector.includesAtom(atoms[i])) {
+          boundingBox.expandByPoint(atoms[i]._position);
+        }
+      }
+    }
+
+    // Build bounding sphere
+    let radiusSquared = 0.0;
+    const center = new THREE.Vector3();
+    boundingBox.getCenter(center);
+    if (n === 1) {
+      // * settings.now.modes.BS.atom); FIXME N: hack commented
+      this._boundaries.boundingSphere.set(center, atoms[0].element.radius);
+    } else {
+      for (let i = 0; i < n; ++i) {
+        if (!selector.includesAtom(atoms[i])) {
+          continue;
+        }
+        const pos = atoms[i]._position;
+        const lengthSquared = center.distanceToSquared(pos);
+        if (radiusSquared < lengthSquared) {
+          radiusSquared = lengthSquared;
+        }
+      }
+      this._boundaries.boundingSphere.set(center, Math.sqrt(radiusSquared));
+    }
+  }
+
+  getTransforms() {
+    return [];
+  }
+
+  getSelector() {
+    return this._selector;
+  }
+
+  getBoundaries() {
+    return this._boundaries;
+  }
+
+  finalize() {
+  }
 }
-
-BiologicalUnit.prototype.constructor = BiologicalUnit;
-
-BiologicalUnit.prototype.computeBoundaries = function() {
-  var atoms = this._complex._atoms;
-  var n = atoms.length;
-  var selector = this._selector;
-  var i;
-
-  var boundingBox = this._boundaries.boundingBox;
-  boundingBox.makeEmpty();
-  if (n === 1) {
-    boundingBox.expandByPoint(atoms[0]._position);
-    const bbc = new THREE.Vector3();
-    boundingBox.getCenter(bbc);
-    var s = 2 * atoms[0].element.radius; //*settings.now.modes.BS.atom; FIXME N: hack commented
-    boundingBox.setFromCenterAndSize(bbc, new THREE.Vector3(s, s, s));
-  } else {
-    for (i = 0; i < n; ++i) {
-      if (selector.includesAtom(atoms[i])) {
-        boundingBox.expandByPoint(atoms[i]._position);
-      }
-    }
-  }
-
-  // Build bounding sphere
-  var radiusSquared = 0.0;
-  const center = new THREE.Vector3();
-  boundingBox.getCenter(center);
-  if (n === 1) {
-    // * settings.now.modes.BS.atom); FIXME N: hack commented
-    this._boundaries.boundingSphere.set(center, atoms[0].element.radius);
-  } else {
-    for (i = 0; i < n; ++i) {
-      if (!selector.includesAtom(atoms[i])) {
-        continue;
-      }
-      var pos = atoms[i]._position;
-      var lengthSquared = center.distanceToSquared(pos);
-      if (radiusSquared < lengthSquared) {
-        radiusSquared = lengthSquared;
-      }
-    }
-    this._boundaries.boundingSphere.set(center, Math.sqrt(radiusSquared));
-  }
-};
-
-BiologicalUnit.prototype.getTransforms = function() {
-  return [];
-};
-
-BiologicalUnit.prototype.getSelector = function() {
-  return this._selector;
-};
-
-BiologicalUnit.prototype.getBoundaries = function() {
-  return this._boundaries;
-};
-
-BiologicalUnit.prototype.finalize = function() {
-};
 
 export default BiologicalUnit;
 
