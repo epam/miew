8ab765b454dcf78f6eadbe580d54183fe8ae1a8e
Nadezhda Katcman <Nadezhda_Katcman@epam.com>
2019-04-22 16:53:39 +0300

    Add unit-tests for toScript function

--- src/options.test.js
+++ src/options.test.js
@@ -44,22 +44,33 @@ describe('options', () => {
     reps: [simplestRep],
   };
   const simpleRepsStr = 'r=0&m=BS&c=EL';
+  const simpleRepsCommands = ['rep 0 m=BS c=EL'];
 
   const bigRepsOpts = {
     reps: [bigRep],
   };
   const bigRepsStr = 'r=0&s=sequence+85,+175,+256+and+name+CB+and+sequence+1:286&m=TX&c=EL&mt=SF';
+  const bigRepsCommands = [
+    'rep 0 s="sequence 85, 175, 256 and name CB and sequence 1:286" m=TX c=EL mt=SF',
+  ];
 
   const paramRepsOpts = {
     preset: 'default',
     reps: [parameterRep],
   };
   const paramRepStr = 'p=default&r=0&m=TR!radius:0.5&c=EL!carbon:0';
+  const paramRepCommands = [
+    'preset default',
+    'rep 0 m=TR radius=0.5 c=EL carbon=0',
+  ];
 
   const subsetRepsSimpleOpts = {
     reps: [subsetRepSimple],
   };
   const subsetRepsSimpleStr = 'r=0&m=CS!subset:chain+A+and+sequence+29+or+serial+196';
+  const subsetRepsSimpleCommands = [
+    'rep 0 m=CS subset="chain A and sequence 29 or serial 196"',
+  ];
 
   const textModeOpts = {
     reps: [{
@@ -71,12 +82,20 @@ describe('options', () => {
 
   const textModeStr = 'r=0&m=TX!template:The+%257B%257BChain%257D%257D.%257B%257BResidue'
     + '%257D%257D.%257B%257BSequence%257D%257D.%257B%257BName%257D%257D';
+  const textModeCommands = [
+    'rep 0 m=TX template="The {{Chain}}.{{Residue}}.{{Sequence}}.{{Name}}"',
+  ];
 
   const multipleRepsOpts = {
     preset: 'default',
     reps: [simplestRep, parameterRep],
   };
   const multipleRepsStr = 'p=default&r=0&m=BS&c=EL&r=1&m=TR!radius:0.5&c=EL!carbon:0';
+  const multipleRepsCommands = [
+    'preset default',
+    'rep 0 m=BS c=EL',
+    'rep 1 m=TR radius=0.5 c=EL carbon=0',
+  ];
 
   /** ******** OBJECTS TEST SETS *********** */
   const objectsList = [
@@ -97,6 +116,10 @@ describe('options', () => {
     _objects: objectsList,
   };
   const objectsStr = 'o=line,A.38.CO1,A.38.CO2&o=line,A.38.CO1,A.38.CO2!color:4291624959,dashSize:0.5';
+  const objectsCommands = [
+    ' line "A.38.CO1" "A.38.CO2"',
+    ' line "A.38.CO1" "A.38.CO2" color=4291624959 dashSize=0.5',
+  ];
 
   const miscOpts = {
     view: '1+n4pwTVeI8Erh8LAHI6CPW63vD40uzs/Ne4ovg==',
@@ -104,6 +127,11 @@ describe('options', () => {
     load: 'mmtf:1crn',
   };
   const miscStr = 'l=mmtf:1crn&u=1&v=1%2Bn4pwTVeI8Erh8LAHI6CPW63vD40uzs/Ne4ovg%3D%3D';
+  const miscCommands = [
+    'load "mmtf:1crn"',
+    'unit 1',
+    'view 1+n4pwTVeI8Erh8LAHI6CPW63vD40uzs/Ne4ovg==',
+  ];
 
   /** ******** SETTINGS TEST SETS *********** */
   const settings = {
@@ -128,6 +156,14 @@ describe('options', () => {
 
   const settingsStr = 'colorers.EL.carbon=-3&colorers.UN.color=16711680&camFov=45'
     + '&camNear=0&resolution=lowest&autoResolution=true';
+  const settingsCommands = [
+    'set colorers.EL.carbon -3',
+    'set colorers.UN.color 16711680',
+    'set camFov 45',
+    'set camNear 0',
+    'set resolution "lowest"',
+    'set autoResolution true',
+  ];
 
   const overallOpts = {
     preset: 'default',
@@ -142,6 +178,21 @@ describe('options', () => {
     + 'and+sequence+29+or+serial+196&o=line,A.38.CO1,A.38.CO2&o=line,A.38.CO1,A.38.CO2!'
     + 'color:4291624959,dashSize:0.5&colorers.EL.carbon=-3&colorers.UN.color=16711680&'
     + 'camFov=45&camNear=0&resolution=lowest&autoResolution=true';
+  const overallCommands = [
+    'preset default',
+    'rep 0 m=BS c=EL',
+    'rep 1 s="sequence 85, 175, 256 and name CB and sequence 1:286" m=TX c=EL mt=SF',
+    'rep 2 m=TR radius=0.5 c=EL carbon=0',
+    'rep 3 m=CS subset="chain A and sequence 29 or serial 196"',
+    ' line "A.38.CO1" "A.38.CO2"',
+    ' line "A.38.CO1" "A.38.CO2" color=4291624959 dashSize=0.5',
+    'set colorers.EL.carbon -3',
+    'set colorers.UN.color 16711680',
+    'set camFov 45',
+    'set camNear 0',
+    'set resolution "lowest"',
+    'set autoResolution true',
+  ];
 
   /** ******** SOPHISTICATED TEST SETS *********** */
   const complexSubsetOpts = {
@@ -152,6 +203,7 @@ describe('options', () => {
     }],
   };
   const complexSubsetStr = 'r=0&m=CS!subset:serial+1%252C13%253A139';
+  const complexSubsetCommands = ['rep 0 m=CS subset="serial 1,13:139"'];
 
   const complexTextModeOpts = {
     reps: [{
@@ -162,6 +214,9 @@ describe('options', () => {
   };
   const complexTextModeStr = 'r=0&m=TX!template:~-%253D!%257B%257BChain%257D%257D.%257B%257B'
     + 'Residue%257D%257D%253A%257B%257BSequence%257D%257D%252C%257B%257BName%257D%257D%252B';
+  const complexTextModeCommands = [
+    'rep 0 m=TX template="~-=!{{Chain}}.{{Residue}}:{{Sequence}},{{Name}}+"',
+  ];
 
   describe('.toURL', () => {
     // extract only options from our URL
@@ -338,6 +393,89 @@ describe('options', () => {
 
     it('restores proper URL for complex text mode opts', () => {
       expect(options.fromURL(urlize(complexTextModeStr))).to.equalOptions(complexTextModeOpts);
+    });
+  });
+
+  describe('.toScript', () => {
+    function equalCommands(original, generated) {
+      if (!_.isArray(original) || original.length !== generated.length + 2) {
+        return false;
+      }
+      if (original[0] !== 'set autobuild false' || original[original.length - 1] !== 'set autobuild true') {
+        return false;
+      }
+      for (let i = 0; i < generated.length; i++) {
+        if (original[i + 1] !== generated[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    chai.use(() => {
+      chai.Assertion.addMethod('equalCommands', function (target) {
+        const source = this._obj;
+        this.assert(
+          equalCommands(target, source),
+          'expected #{this} to equal #{exp}',
+          'expected #{this} to not equal #{exp}',
+          target,
+          source,
+          true,
+        );
+      });
+    });
+
+    function toCommands(script) {
+      return script.split('\n');
+    }
+
+    it('generates proper script for simplest rep lists', () => {
+      expect(simpleRepsCommands).to.equalCommands(toCommands(options.toScript(simpleRepsOpts)));
+      expect(bigRepsCommands).to.equalCommands(toCommands(options.toScript(bigRepsOpts)));
+    });
+
+    it('restores mode/colorer options', () => {
+      expect(paramRepCommands).to.equalCommands(toCommands(options.toScript(paramRepsOpts)));
+    });
+
+    it('restores string params', () => {
+      expect(subsetRepsSimpleCommands).to.equalCommands(toCommands(options.toScript(subsetRepsSimpleOpts)));
+    });
+
+    it('restores multi rep lists', () => {
+      expect(multipleRepsCommands).to.equalCommands(toCommands(options.toScript(multipleRepsOpts)));
+    });
+
+    it('restores text mode options', () => {
+      expect(textModeCommands).to.equalCommands(toCommands(options.toScript(textModeOpts)));
+    });
+
+    it('restores objects', () => {
+      expect(objectsCommands).to.equalCommands(toCommands(options.toScript(objectsOpts)));
+    });
+
+    it('restores high level properties', () => {
+      expect(miscCommands).to.equalCommands(toCommands(options.toScript(miscOpts)));
+    });
+
+    // settings params
+    it('restores proper script for settings', () => {
+      expect(settingsCommands).to.equalCommands(toCommands(options.toScript(settingsOpts)));
+    });
+
+    // overall
+    it('restores proper scripts', () => {
+      expect(overallCommands).to.equalCommands(toCommands(options.toScript(overallOpts)));
+    });
+
+    // sophisticated examples
+    it('restores proper script for complex subset opts', () => {
+      expect(complexSubsetCommands).to.equalCommands(toCommands(options.toScript(complexSubsetOpts)));
+    });
+
+    it('restores proper script for complex text mode opts', () => {
+      expect(complexTextModeCommands).to.equalCommands(toCommands(options.toScript(complexTextModeOpts)));
     });
   });
 });
