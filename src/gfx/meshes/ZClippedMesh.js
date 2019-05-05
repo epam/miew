4f5ae4d417234b4b1812f70ee4ce23ab9dc76e35
Demian Lesik <Demian_Lesik@epam.com>
2019-05-05 14:58:03 +0300

    Remove repeated creation of 'new' verctors/matrices in ZClippedMesh

--- src/gfx/meshes/ZClippedMesh.js
+++ src/gfx/meshes/ZClippedMesh.js
@@ -11,24 +11,29 @@ class ZClippedMesh extends Mesh {
   }
 
   _onBeforeRender(renderer, scene, camera) {
-    Mesh.prototype._onBeforeRender.call(this, renderer, scene, camera);
+    const _mvLength = new THREE.Vector3();
+    const _center = new THREE.Vector3();
+    const _modelView = new THREE.Matrix4();
 
-    const geo = this.geometry;
-    const { material } = this;
-    if (!geo.zClip || !material.uberOptions) {
-      return;
-    }
+    return function () {
+      Mesh.prototype._onBeforeRender.call(this, renderer, scene, camera);
 
-    const zClipCoef = 0.5;
-    // TODO remove these instantiations
-    const modelView = new THREE.Matrix4().multiplyMatrices(this.matrixWorld, camera.matrixWorldInverse);
-    const scale = new THREE.Vector3().setFromMatrixColumn(modelView, 0);
-    const s = scale.length();
+      const geo = this.geometry;
+      const { material } = this;
+      if (!geo.zClip || !material.uberOptions) {
+        return;
+      }
 
-    const center = new THREE.Vector3().copy(geo.boundingSphere.center);
-    this.localToWorld(center);
-    material.uberOptions.zClipValue = camera.position.z - center.z
-      - s * (zClipCoef * geo.boundingSphere.radius);
+      const zClipCoef = 0.5;
+
+      const modelView = _modelView.multiplyMatrices(this.matrixWorld, camera.matrixWorldInverse);
+      const s = _mvLength.setFromMatrixColumn(modelView, 0).length();
+      const center = _center.copy(geo.boundingSphere.center);
+
+      this.localToWorld(center);
+      material.uberOptions.zClipValue = camera.position.z - center.z
+        - s * (zClipCoef * geo.boundingSphere.radius);
+    };
   }
 }
 
