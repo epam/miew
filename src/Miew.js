3aab9b8d16667696df5e0c681f36971c4909a497
NataliaSmirnova <Natalia_Smirnova@epam.com>
2019-05-14 14:53:41 +0300

    Improve the code a little

--- src/Miew.js
+++ src/Miew.js
@@ -1428,43 +1428,9 @@ Miew.prototype._performAO = (function () {
   const _horBlurMaterial = new ao.HorBilateralBlurMaterial();
   const _vertBlurMaterial = new ao.VertBilateralBlurMaterial();
 
-  const _samplesKernel = [
-    // hemisphere samples adopted to sphere
-    new THREE.Vector3(0.295184, 0.077723, 0.068429),
-    new THREE.Vector3(-0.271976, -0.365221, 0.838363),
-    new THREE.Vector3(0.547713, 0.467576, 0.488515),
-    new THREE.Vector3(0.662808, -0.031733, 0.584758),
-    new THREE.Vector3(-0.025717, 0.218955, 0.657094),
-    new THREE.Vector3(-0.310153, -0.365223, 0.370701),
-    new THREE.Vector3(-0.101407, -0.006313, 0.747665),
-    new THREE.Vector3(-0.769138, 0.360399, 0.086847),
-    new THREE.Vector3(-0.271988, -0.275140, 0.905353),
-    new THREE.Vector3(0.096740, -0.566901, 0.700151),
-    new THREE.Vector3(0.562872, -0.735136, 0.094647),
-    new THREE.Vector3(0.379877, 0.359278, 0.190061),
-    new THREE.Vector3(0.519064, -0.023055, 0.405068),
-    new THREE.Vector3(-0.301036, 0.114696, 0.088885),
-    new THREE.Vector3(-0.282922, 0.598305, 0.487214),
-    new THREE.Vector3(-0.181859, 0.251670, 0.679702),
-    new THREE.Vector3(-0.191463, -0.635818, 0.512919),
-    new THREE.Vector3(-0.293655, 0.427423, 0.078921),
-    new THREE.Vector3(-0.267983, 0.680534, 0.132880),
-    new THREE.Vector3(0.139611, 0.319637, 0.477439),
-    new THREE.Vector3(-0.352086, 0.311040, 0.653913),
-    new THREE.Vector3(0.321032, 0.805279, 0.487345),
-    new THREE.Vector3(0.073516, 0.820734, 0.414183),
-    new THREE.Vector3(-0.155324, 0.589983, 0.411460),
-    new THREE.Vector3(0.335976, 0.170782, 0.527627),
-    new THREE.Vector3(0.463460, -0.355658, 0.167689),
-    new THREE.Vector3(0.222654, 0.596550, 0.769406),
-    new THREE.Vector3(0.922138, -0.042070, 0.147555),
-    new THREE.Vector3(-0.727050, -0.329192, 0.369826),
-    new THREE.Vector3(-0.090731, 0.533820, 0.463767),
-    new THREE.Vector3(-0.323457, -0.876559, 0.238524),
-    new THREE.Vector3(-0.663277, -0.372384, 0.342856),
-  ];
-  // var _kernelOffsets = [-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0];
-  const _kernelOffsets = [-2.0, -1.0, 0.0, 1.0, 2.0];
+  const _translation = new THREE.Vector3();
+  const _quaternion = new THREE.Quaternion();
+  const _scale = new THREE.Vector3();
 
   return function (srcColorBuffer, normalBuffer, srcDepthTexture, targetBuffer, tempBuffer, tempBuffer1) {
     if (!srcColorBuffer || !normalBuffer || !srcDepthTexture || !targetBuffer || !tempBuffer || !tempBuffer1) {
@@ -1473,11 +1439,6 @@ Miew.prototype._performAO = (function () {
 
     const self = this;
     const gfx = self._gfx;
-
-    // clear canvasFMatrix4
-    // gfx.renderer.setClearColor(THREE.aliceblue, 1);
-    // gfx.renderer.setRenderTarget(targetBuffer);
-    // gfx.renderer.clear(true, false);
 
     // do fxaa processing of offscreen buff2
     _aoMaterial.uniforms.diffuseTexture.value = srcColorBuffer.texture;
@@ -1488,12 +1449,8 @@ Miew.prototype._performAO = (function () {
     _aoMaterial.uniforms.projMatrix.value = gfx.camera.projectionMatrix;
     _aoMaterial.uniforms.aspectRatio.value = gfx.camera.aspect;
     _aoMaterial.uniforms.tanHalfFOV.value = Math.tan(THREE.Math.DEG2RAD * 0.5 * gfx.camera.fov);
-    _aoMaterial.uniforms.samplesKernel.value = _samplesKernel;
-    const translation = new THREE.Vector3();
-    const quaternion = new THREE.Quaternion();
-    const scale = new THREE.Vector3();
-    gfx.root.matrix.decompose(translation, quaternion, scale);
-    _aoMaterial.uniforms.kernelRadius.value = settings.now.debug.ssaoKernelRadius * scale.x;
+    gfx.root.matrix.decompose(_translation, _quaternion, _scale);
+    _aoMaterial.uniforms.kernelRadius.value = settings.now.debug.ssaoKernelRadius * _scale.x;
     _aoMaterial.uniforms.depthThreshold.value = 2.0 * this._getBSphereRadius(); // diameter
     _aoMaterial.uniforms.factor.value = settings.now.debug.ssaoFactor;
     _aoMaterial.uniforms.noiseTexture.value = noise.noiseTexture;
@@ -1502,7 +1459,6 @@ Miew.prototype._performAO = (function () {
     if (fog) {
       _aoMaterial.uniforms.fogNearFar.value.set(fog.near, fog.far);
     }
-    _aoMaterial.transparent = false;
     // N: should be tempBuffer1 for proper use of buffers (see buffers using outside the function)
     gfx.renderer.setRenderTarget(tempBuffer1);
     gfx.renderer.renderScreenQuad(_aoMaterial);
@@ -1510,7 +1466,6 @@ Miew.prototype._performAO = (function () {
     _horBlurMaterial.uniforms.aoMap.value = tempBuffer1.texture;
     _horBlurMaterial.uniforms.srcTexelSize.value.set(1.0 / tempBuffer1.width, 1.0 / tempBuffer1.height);
     _horBlurMaterial.uniforms.depthTexture.value = srcDepthTexture;
-    _horBlurMaterial.uniforms.samplesOffsets.value = _kernelOffsets;
     gfx.renderer.setRenderTarget(tempBuffer);
     gfx.renderer.renderScreenQuad(_horBlurMaterial);
 
@@ -1518,7 +1473,6 @@ Miew.prototype._performAO = (function () {
     _vertBlurMaterial.uniforms.diffuseTexture.value = srcColorBuffer.texture;
     _vertBlurMaterial.uniforms.srcTexelSize.value.set(1.0 / tempBuffer.width, 1.0 / tempBuffer.height);
     _vertBlurMaterial.uniforms.depthTexture.value = srcDepthTexture;
-    _vertBlurMaterial.uniforms.samplesOffsets.value = _kernelOffsets;
     gfx.renderer.setRenderTarget(targetBuffer);
     gfx.renderer.renderScreenQuad(_vertBlurMaterial);
   };
