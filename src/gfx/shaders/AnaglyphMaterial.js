7bb08a8b6b03ac818e46e54d822a0d7f9e050457
NataliaSmirnova <Natalia_Smirnova@epam.com>
2019-05-14 14:58:41 +0300

    Make class (es2015)

--- src/gfx/shaders/AnaglyphMaterial.js
+++ src/gfx/shaders/AnaglyphMaterial.js
@@ -4,33 +4,22 @@ import * as THREE from 'three';
 import vertexShader from './ScreenQuad_vert.glsl';
 import fragmentShader from './Anaglyph_frag.glsl';
 
-const defaultUniforms = THREE.UniformsUtils.merge([
-  {
-    srcL: { type: 't', value: null },
-    srcR: { type: 't', value: null },
-  },
-]);
-
-function overrideUniforms(params) {
-  const uniforms = THREE.UniformsUtils.clone(defaultUniforms);
-  for (const p in params) {
-    if (uniforms.hasOwnProperty(p)) {
-      uniforms[p].value = params[p];
-    }
+class AnaglyphMaterial extends THREE.RawShaderMaterial {
+  constructor() {
+    super();
+    const settings = {
+      uniforms: {
+        srcL: { type: 't', value: null },
+        srcR: { type: 't', value: null },
+      },
+      vertexShader,
+      fragmentShader,
+      transparent: false,
+      depthTest: false,
+      depthWrite: false,
+    };
+    this.setValues(settings);
   }
-  return uniforms;
-}
-
-function AnaglyphMaterial(params) {
-  const settings = {
-    uniforms: overrideUniforms(params),
-    vertexShader,
-    fragmentShader,
-    transparent: false,
-    depthTest: false,
-    depthWrite: false,
-  };
-  return new THREE.RawShaderMaterial(settings);
 }
 
 export default AnaglyphMaterial;
