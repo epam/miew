49626ab939d838f6d6bb115b28af0dce4eee713b
Nadezhda Katcman <Nadezhda_Katcman@epam.com>
2019-05-21 18:44:15 +0300

    Add tests for RangeList

--- src/chem/selectors.test.js
+++ src/chem/selectors.test.js
@@ -48,4 +48,62 @@ describe('selectors', () => {
       });
     });
   });
+
+  describe('class RangeList', () => {
+    const range0 = new selectors.Range(2, 8);
+    const range1 = new selectors.Range(1, 14);
+    const range2 = new selectors.Range(10, 12);
+
+    const emptyRL = new selectors.RangeList();
+    const oneRangeRL = new selectors.RangeList(range0);
+    const twoRangesRL = new selectors.RangeList([range0, range2]);
+    const threeRangesRL = new selectors.RangeList([range0, range1, range2]);
+
+    describe('contructor', () => {
+      it('no arguments', () => {
+        expect(emptyRL).to.deep.equal({_values: []});
+      });
+      it('from one range', () => {
+        expect(oneRangeRL).to.deep.equal({_values: [range0]});
+      });
+      it('from array of ranges', () => {
+        expect(threeRangesRL).to.deep.equal({_values: [range0, range1, range2]});
+      });
+    });
+    describe('append', () => {
+      it('append range', () => {
+        expect(oneRangeRL.append(range2)).to.deep.equal(twoRangesRL);
+      });
+    });
+    describe('remove', () => {
+      it('exist range', () => {
+        expect(threeRangesRL.remove(range1)).to.deep.equal(twoRangesRL);
+      });
+      it('fantastic range', () => {
+        expect(twoRangesRL.remove(range1)).to.deep.equal(twoRangesRL);
+      });
+    });
+    describe('toString', () => {
+      it('toString', () => {
+        expect(twoRangesRL.toString()).to.equal(range0.toString() + ',' + range2.toString());
+      });
+    });
+    describe('toJSON', () => {
+      it('toJSON', () => {
+        expect(twoRangesRL.toJSON()).to.deep.equal([range0.toJSON(), range2.toJSON()]);
+      });
+    });
+    describe('includes', () => {
+      it('in one of ranges', () => {
+        expect(twoRangesRL.includes(11)).to.equal(true);
+      });
+      it('in two of ranges', () => {
+        expect(threeRangesRL.includes(11)).to.equal(true);
+      });
+      it('outside of ranges', () => {
+        expect(twoRangesRL.includes(9)).to.equal(false);
+        expect(twoRangesRL.includes(20)).to.equal(false);
+      });
+    });
+  });
 });
