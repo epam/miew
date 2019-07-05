1646e0dbfbee5753748f7bd9749b9b091ce6ae73
Pavel Smirnov <Pavel_Smirnov1@epam.com>
2019-07-05 12:11:30 +0300

    Extract CIF file reader routine from the CIFParser class

---
+++ src/io/parsers/readCIF.test.js
@@ -0,0 +1,11 @@
+import chai, { expect } from 'chai';
+import dirtyChai from 'dirty-chai';
+import readCIF from './readCIF';
+
+chai.use(dirtyChai);
+
+describe('readCIF()', () => {
+  it('accepts empty file', () => {
+    expect(readCIF('')).to.deep.equal({ data: {} });
+  });
+});
