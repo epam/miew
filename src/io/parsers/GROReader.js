598b5deefd74b15ee7525066b85e7a03a6612b0b
Dmitriy Bragin <dmitriybragin@yandex.ru>
2019-08-05 14:56:04 +0300

    Add docs for PDBStream and tests for GROReader class

--- src/io/parsers/GROReader.js
+++ src/io/parsers/GROReader.js
@@ -1,34 +1,19 @@
+import PDBStream from './PDBStream';
+
 /**
- * Little helper class. Code is EXACT copy of PDBStream except for it's name.
+ * Little helper class for GRO Parser usage.
  *
  * @param {String} data      - Input file.
- * @param {Number} start     - Starting position of line.
- * @param {Number} nextCR    - Position of next CR (0x0D).
- * @param {Number} nextLF    - Position of next LF (0x0A).
  * @param {Number} next      - End position of line.
- * @param {Number} end       - End of data.
  *
  * @exports GROReader
  * @constructor
  */
-export default class GROReader {
+export default class GROReader extends PDBStream {
   constructor(data) {
-    this._data = data;
-    this._start = 0;
-    this._nextCR = -1;
-    this._nextLF = -1;
+    super(data);
     this._next = -1;
-    this._end = data.length;
-
     this.next();
-  }
-
-  /**
-   * Reading next line.
-   * @returns {String} Next line in data (ending with LF or CR)
-   */
-  readLine() {
-    return this._data.slice(this._start, this._next);
   }
 
   /**
@@ -37,85 +22,5 @@ export default class GROReader {
    */
   getNext() {
     return this._next;
-  }
-
-  /**
-   * Reading character from position.
-   * @param{Number} pos - Position in current line.
-   * @returns {String} Character from position
-   */
-  readChar(pos) {
-    pos = this._start + pos - 1;
-    return pos < this._next ? this._data[pos] : ' ';
-  }
-
-  /**
-   * Reading character code from position.
-   * @param{Number} pos - Position in current line.
-   * @returns {String} Character code from position
-   */
-  readCharCode(pos) {
-    pos = this._start + pos - 1;
-    return pos < this._next ? this._data.charCodeAt(pos) : 32;
-  }
-
-  /**
-   * Reading string from begin to end points.
-   * For a reason unknown, numbering assumed not to start from 0, but from 1.
-   * @param{Number} begin - Begin point in current line.
-   * @param{Number} end - End point in current line.
-   * @returns {String} String from begin to end
-   */
-  readString(begin, end) {
-    const from = this._start + begin - 1;
-    const to = this._start + end;
-    return this._data.slice(from, to < this._next ? to : this._next);
-  }
-
-  /**
-   * Reading integer from begin to end points.
-   * @param{Number} begin - Begin point in current line.
-   * @param{Number} end - End point in current line.
-   * @returns {Number} Integer from begin to end
-   */
-  readInt(begin, end) {
-    return parseInt(this.readString(begin, end), 10);
-  }
-
-  /**
-   * Reading float from begin to end points.
-   * @param{Number} begin - Begin point in current line.
-   * @param{Number} end - End point in current line.
-   * @returns {Number} Float from begin to end
-   */
-  readFloat(begin, end) {
-    return parseFloat(this.readString(begin, end));
-  }
-
-  /**
-   * Checking for end of data.
-   * @returns {boolean} True if data is ended, false otherwise
-   */
-  end() {
-    return this._start >= this._end;
-  }
-
-  /**
-   * Procedure to re-arrange current pointers in data.
-   */
-  next() {
-    const start = this._next + 1;
-    this._start = start < this._end ? start : this._end;
-
-    // support CR, LF, CR+LF line endings
-    // do not support LF+CR, CR+CR+LF, and other strange combinations
-
-    if (this._start > this._nextCR) {
-      this._nextCR = (this._data.indexOf('\r', this._start) + 1 || this._end + 1) - 1;
-    }
-    if (this._start > this._nextLF) {
-      this._nextLF = (this._data.indexOf('\n', this._start) + 1 || this._end + 1) - 1;
-    }
-    this._next = this._nextCR + 1 < this._nextLF ? this._nextCR : this._nextLF;
   }
 }
--- test/functional/data/complexTestData.js
+++ test/functional/data/complexTestData.js
@@ -10,28 +10,35 @@ export default [{
     molecules: 1,
     units: { xyz: 1, gro: 1, default: 2 },
     symmetries: { pdb: 2 },
-    helices: { mmtf: 3, xyz: 0, gro: 0, default: 2 },
-    sheets: { mmtf: 2, xyz: 0, gro: 0, default: 1 },
+    helices: {
+      mmtf: 3,
+      xyz: 0,
+      gro: 0,
+      default: 2,
+    },
+    sheets: {
+      mmtf: 2, xyz: 0, gro: 0, default: 1,
+    },
   },
 }, {
   // hydrogens, waters, ligands, altloc, all aminoacids, biomolecules, symmetry, ANISOU, LINK
   name: '4NRE',
-  formats: ['pdb', 'cif', 'mmtf', 'gro'],
+  formats: ['pdb', 'cif', 'mmtf'],
   num: {
     atoms: 11306,
     bonds: { pdb: 11093, cif: 11083, mmtf: 11066 }, // = 11093 in VMD, but they are not all valid
     residues: 1063, // =  1068 in VMD!!!
     chains: 1,
-    molecules: { pdb: 1, gro: 1,default: 7 },
-    units: { gro: 1, default: 3},
-    symmetries: { pdb: 18, gro: 1},
+    molecules: { pdb: 1, default: 7 },
+    units: 3,
+    symmetries: { pdb: 18 },
     helices: { mmtf: 34, default: 33 },
     sheets: { mmtf: 19, default: 6 },
   },
 }, {
   // lots of chains, models (+hydrogens)
   name: '2MPZ',
-  formats: ['pdb', 'cif', 'mmtf', 'gro'],
+  formats: ['pdb', 'cif', 'mmtf'],
   num: {
     atoms: 10422,
     bonds: { mmtf: 10395, default: 10449 },
@@ -46,7 +53,7 @@ export default [{
 }, {
   // virus = biomolecule with lots of transformations, symmetry
   name: '1MVA',
-  formats: ['pdb', 'cif', 'mmtf', 'gro'],
+  formats: ['pdb', 'cif', 'mmtf'],
   num: {
     atoms: 3112,
     bonds: 2940,
@@ -61,7 +68,7 @@ export default [{
 }, {
   // DNA, molecules
   name: '5B40',
-  formats: ['pdb', 'cif', 'mmtf', 'gro'],
+  formats: ['pdb', 'cif', 'mmtf'],
   num: {
     atoms: 11817,
     bonds: 12621,
