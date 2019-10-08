acd794e08bc3110fcf1b5cfb67879ef8a1fc946b
Demian Lesik <Demian_Lesik@epam.com>
2019-10-08 17:14:39 +0300

    Fix "Trailing spaces" issue

--- src/settings.test.js
+++ src/settings.test.js
@@ -29,7 +29,7 @@ describe('/settings', () => {
   describe('.get()', () => {
     it('gets current parameter', () => {
       const testSettings = _.cloneDeep(settings);
-      testSettings.now.modes.VD.kSigma = 1;    
+      testSettings.now.modes.VD.kSigma = 1;
       expect(settings.get('modes.VD.kSigma')).to.be.equal(1);
     });
   });
