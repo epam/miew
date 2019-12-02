c830dcb9a536c0016f9b67bdaea5273b84693818
NataliaSmirnova <Natalia_Smirnova@epam.com>
2019-12-02 16:22:36 +0300

    Add example of react application

---
+++ examples/miew_via_react/controls/Checkbox.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class Checkbox extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    let value = event.target.checked;
+    console.log(`checkbox updated: value ${value}`);
+    if (this.props.viewer) {
+      value = (value !== undefined) ? this.props.prefType(value) : this.props.prefType();
+      if (!Number.isNaN(value)) {
+        this.props.viewer.set(this.props.prefName, value);
+      }
+    }
+  }
+
+  render() {
+    const value = (this.props.viewer && this.props.prefType(this.props.viewer.get(this.props.prefName))) || this.props.prefType();
+    console.log('changes checkbox');
+    return <input type='checkbox' checked={ value } onChange={ this.onChange } />;
+  }
+}
