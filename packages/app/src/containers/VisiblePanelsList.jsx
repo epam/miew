eab7dce5544d9a15cf7dae0f8cad1e90086f036b
NataliaSmirnova <Natalia_Smirnova@epam.com>
2020-06-05 16:27:11 +0300

    Extend eslint to *.jsx

--- packages/app/src/containers/VisiblePanelsList.jsx
+++ packages/app/src/containers/VisiblePanelsList.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PanelsList from '../components/menu/panelslist/PanelsList.jsx';
 
 const mapStateToProps = (state) => ({
-  visibility: state.visiblePanels.visibility ? 'visible' : 'hidden' ,
+  visibility: state.visiblePanels.visibility ? 'visible' : 'hidden',
 });
 
 const VisiblePanelsList = connect(
