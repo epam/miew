c1d009ba3b4453cd96b466969da271cfebb77893
AlinaFirsova <alina_firsova@epam.com>
2020-11-23 14:47:12 +0300

    Remove TODO from visiblePanels

--- packages/app/src/store/visiblePanels.js
+++ packages/app/src/store/visiblePanels.js
@@ -2,7 +2,7 @@ import ACTIONS from '../constants';
 
 const visiblePanels = (state = {}, action) => {
   switch (action.type) {
-    case ACTIONS.SHOW_NAV: // TODO rename?
+    case ACTIONS.SHOW_NAV:
       return { ...state, visibility: !state.visibility };
     default:
       return state;
