776af1ac87c5082b565fc02772b3a52d7f1dc2af
Anastasiia Moskvina <anastasiia_moskvina@epam.com>
2021-12-09 15:26:43 +0300

    add useResizeObserver to Miew component

--- packages/miew-react/rollup.config.js
+++ packages/miew-react/rollup.config.js
@@ -62,7 +62,6 @@ const config = {
         'process.env.BUILD_DATE': JSON.stringify(
           new Date().toISOString().slice(0, 19)
         ),
-        //TODO: add logic to init BUILD_NUMBER
         'process.env.BUILD_NUMBER': JSON.stringify(undefined),
         'process.env.HELP_LINK': JSON.stringify(initLink(branchName()))
       },
--- packages/miew-react/src/Miew.tsx
+++ packages/miew-react/src/Miew.tsx
@@ -1,10 +1,25 @@
+import useResizeObserver from "use-resize-observer"
+import clsx from 'clsx'
 import classes from './Miew.module.scss'
 
+const mediaSizes = {
+  smallWidth: 800,
+  smallHeight: 400
+}
+
 function Miew(props) {
+
+  const { ref, width, height } = useResizeObserver<HTMLDivElement>();
+
   return (
-    <div className={classes.miew}>
+    <div className={clsx(classes.miew, {
+      [classes.small]:
+        (height && height <= mediaSizes.smallHeight) ||
+        (width && width <= mediaSizes.smallWidth)
+    })}ref={ref}>
       <p>HELLO, I'm Miew!</p>
       <p>{props.text}</p>
+      <p>Width: {width} Height: {height}</p>
     </div>
   );
 }
