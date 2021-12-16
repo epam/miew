233325bba4a116f927b9b0a083f315bd2e57ad29
Anastasiia Moskvina <anastasiia_moskvina@epam.com>
2021-12-16 10:56:32 +0300

    update dist, change hasOwnProperty check

--- packages/lib/src/options.js
+++ packages/lib/src/options.js
@@ -253,29 +253,26 @@ function _fromArray(entries) {
     const /** string[] */ entry = entries[i]
     let /** string? */ key = entry[0]
     const /** string? */ value = entry[1]
+    let /** function|string? */ action = actions[key]
 
-    if (actions.hasOwnProperty(key)) {
-      let /** function|string? */ action = actions[key]
+    // unwind shortcuts and aliases
+    while (_.isString(action)) {
+      key = action
+      action = actions[key]
+    }
 
-      // unwind shortcuts and aliases
-      while (_.isString(action)) {
-        key = action
-        action = actions[key]
+    // either set a property or use specialized parser
+    if (!action) {
+      const adapter = adapters[typeof _.get(settings.defaults, key)]
+      if (adapter) {
+        _.set(opts, `settings.${key}`, adapter(value))
+      } else {
+        logger.warn(`Unknown option "${key}"`)
       }
-
-      // either set a property or use specialized parser
-      if (!action) {
-        const adapter = adapters[typeof _.get(settings.defaults, key)]
-        if (adapter) {
-          _.set(opts, `settings.${key}`, adapter(value))
-        } else {
-          logger.warn(`Unknown option "${key}"`)
-        }
-      } else if (typeof action === 'function') {
-        const result = action(value, opts)
-        if (result !== undefined) {
-          opts[key] = result
-        }
+    } else if (typeof action === 'function' && actions.hasOwnProperty(key)) {
+      const result = action(value, opts)
+      if (result !== undefined) {
+        opts[key] = result
       }
     }
   }
