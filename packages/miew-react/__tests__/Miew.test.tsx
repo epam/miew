3b15740f3b7289d0b43f5370b81dd4587dcbf8ca
Anastasiia Moskvina <77681522+AnaMoskvina@users.noreply.github.com>
2021-12-22 14:32:21 +0300

    #360 Configure miew-react workspace (#363)
    * configure miew-react workspace
    
    * update yarn.lock
    
    * update configuration for testing
    
    * add todo about mocks folder
    
    * remove FC typing
    
    Co-authored-by: Anastasiia Moskvina <anastasiia_moskvina@epam.com>

---
+++ packages/miew-react/__tests__/Miew.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import { Miew } from 'src/Miew'
+
+// TODO create mock folder, and move all mocks there
+jest.mock('use-resize-observer', () => jest.fn().mockReturnValue({ ref: null, width: 100, height: 100 }))
+
+describe('Miew component', () => {
+  it('should be rendered', () => {
+    expect(render(<Miew text="test" />)).toMatchSnapshot()
+  })
+})
--- packages/miew-react/src/Miew.tsx
+++ packages/miew-react/src/Miew.tsx
@@ -2,25 +2,29 @@ import useResizeObserver from 'use-resize-observer'
 import clsx from 'clsx'
 import classes from './Miew.module.scss'
 
-const mediaSizes = {
+const MEDIA_SIZES = {
   smallWidth: 800,
   smallHeight: 400
 }
 
-function Miew(props) {
+type Props = {
+  text: string
+}
+
+function Miew({ text }: Props) {
   const { ref, width, height } = useResizeObserver<HTMLDivElement>()
 
   return (
     <div
       className={clsx(classes.miew, {
         [classes.small]:
-          (height && height <= mediaSizes.smallHeight) ||
-          (width && width <= mediaSizes.smallWidth)
+          (height && height <= MEDIA_SIZES.smallHeight) ||
+          (width && width <= MEDIA_SIZES.smallWidth)
       })}
       ref={ref}
     >
       <p>HELLO, I am Miew!</p>
-      <p>{props.text}</p>
+      <p>{text}</p>
       <p>
         Width: {width} Height: {height}
       </p>
