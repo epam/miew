04f8fc23ed89a751b90259cd4f09f6a1733e557b
Anastasiia Moskvina <anastasiia_moskvina@epam.com>
2022-02-24 16:50:07 +0300

    update test

--- packages/miew-react/src/Viewer.test.tsx
+++ packages/miew-react/src/Viewer.test.tsx
@@ -40,6 +40,8 @@ const customTheme = {
   }
 }
 
+const { MINIMAL, CUSTOM, DEFAULT } = ViewerMode
+
 describe('Viewer component', () => {
   it('should apply custom theme when theme prop is provided', () => {
     expect(render(<Viewer theme={customTheme} />).container).toMatchSnapshot()
@@ -48,18 +50,12 @@ describe('Viewer component', () => {
     expect(render(<Viewer />).container).toMatchSnapshot()
   })
   it('should not contain Control panel, if "minimal" mode prop is provided', () => {
-    expect(
-      render(<Viewer mode={ViewerMode.MINIMAL} />).container
-    ).toMatchSnapshot()
+    expect(render(<Viewer mode={MINIMAL} />).container).toMatchSnapshot()
   })
   it('should contain Control panel, if "default" mode prop is provided', () => {
-    expect(
-      render(<Viewer mode={ViewerMode.DEFAULT} />).container
-    ).toMatchSnapshot()
+    expect(render(<Viewer mode={DEFAULT} />).container).toMatchSnapshot()
   })
   it('should contain Control panel, if "custom" mode prop is provided', () => {
-    expect(
-      render(<Viewer mode={ViewerMode.CUSTOM} />).container
-    ).toMatchSnapshot()
+    expect(render(<Viewer mode={CUSTOM} />).container).toMatchSnapshot()
   })
 })
