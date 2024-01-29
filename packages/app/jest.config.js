46ee75a63d7a11c2e0b05901324fc4de2328b303
AlinaFirsovaRaft <alina.firsova@raftds.com>
2024-01-29 14:31:21 +0300

    Tests are broken in the react app

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,19 +1,14 @@
 module.exports = {
   verbose: true,
   transform: {
-    '^.+\\.jsx?$': './tools/babel-jest-wrapper.js',
+    "^.+\\.jsx?$": "./tools/babel-jest-wrapper.js",
   },
-  reporters: [
-    ['jest-simple-dot-reporter', { color: true }],
-  ],
-  collectCoverageFrom: [
-    'src/**/*.jsx',
-  ],
-  moduleDirectories: [
-    'node_modules',
-  ],
-  coverageReporters: [
-    'lcov', 'text-summary',
-  ],
-  coverageDirectory: 'coverage',
+  reporters: [["jest-simple-dot-reporter", { color: true }]],
+  collectCoverageFrom: ["src/**/*.jsx"],
+  moduleDirectories: ["node_modules"],
+  coverageReporters: ["lcov", "text-summary"],
+  coverageDirectory: "coverage",
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };
