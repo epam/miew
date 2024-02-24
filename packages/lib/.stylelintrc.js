b52c559c51bfe32d67e007c079f417bf7e885c91
Natalia Smirnova <>
2024-02-23 23:08:49 -0500

    Update stylelint

--- packages/lib/.stylelintrc.js
+++ packages/lib/.stylelintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'no-descending-specificity': null,
+    'color-function-notation': 'legacy',
+    'selector-attribute-quotes': 'never',
+    'alpha-value-notation': 'number',
+    'property-no-vendor-prefix': null,
+    'declaration-block-no-redundant-longhand-properties': null,
+    'scss/at-import-partial-extension': 'always',
+  },
 };
