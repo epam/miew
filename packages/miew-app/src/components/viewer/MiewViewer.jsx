94a45ba7432615b62f9b7bc88cd99d73259b6393
Pavel Smirnov <pavel_smirnov1@epam.com>
2025-09-10 15:01:22 -0400

    Remove all non-serializable data from store
    Now that MiewContext provides direct access to viewer instance, remove
    all redundant data duplication from Redux state:
    
    - Remove SEND_INFO, SAVE_COLORERS, SAVE_MODES actions
    - Components now get complex info, colorers, and modes directly from
      viewer via useMiew() hook instead of Redux state
    - Redux state now only contains pure UI state (loadingStage, theme)
    - Eliminates all Redux best practices violations
    - Significantly reduces state complexity and memory usage
    
    This completes the Redux refactor - state now contains only serializable
    UI data while all domain data is accessed directly from the viewer.

--- packages/miew-app/src/components/viewer/MiewViewer.jsx
+++ packages/miew-app/src/components/viewer/MiewViewer.jsx
@@ -7,7 +7,7 @@ import { MiewProvider } from '../../contexts/MiewContext';
 
 let viewer = null;
 export default function MiewViewer({
-  frozen, onChange, updateLoadingStage, sendInfo, saveColorers, saveModes, children,
+  frozen, onChange, updateLoadingStage, children,
 }) {
   const domElement = useRef();
   const _onChange = (prefs) => {
@@ -36,10 +36,6 @@ export default function MiewViewer({
       updateLoadingStage(e.data);
     });
     viewer.logger.level = 'debug';
-    const { colorers } = Miew;
-    saveColorers(colorers);
-    const { modes } = Miew;
-    saveModes(modes);
 
     viewer.settings.addEventListener('change:axes', _onChange);
     viewer.settings.addEventListener('change:autoRotation', _onChange);
@@ -47,15 +43,6 @@ export default function MiewViewer({
     if (viewer.init()) {
       viewer.run();
     }
-    viewer.addEventListener('buildingDone', () => {
-      const names = viewer.getVisuals();
-      const ids = [];
-      for (let i = 0; i < names.length; i++) {
-        const complex = viewer._getComplexVisual(names[i]).getComplex();
-        ids.push(complex);
-      }
-      sendInfo(ids);
-    });
     return removeViewer;
   }, []);
 
--- packages/miew-app/src/containers/MiewViewerContainer.js
+++ packages/miew-app/src/containers/MiewViewerContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 
 import MiewViewer from '../components/viewer/MiewViewer.jsx';
 import {
-  sendInfo, updateLoadingStage, saveColorers, saveModes,
+  updateLoadingStage,
 } from '../actions';
 
 const mapStateToProps = (state) => ({
@@ -10,10 +10,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  sendInfo: (ids) => dispatch(sendInfo(ids)),
   updateLoadingStage: (title) => dispatch(updateLoadingStage(title)),
-  saveColorers: (colorers) => dispatch(saveColorers(colorers)),
-  saveModes: (modes) => dispatch(saveModes(modes)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MiewViewer);
