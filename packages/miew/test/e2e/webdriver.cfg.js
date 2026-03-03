8e5fc90059c43b73c94d59acdcd983d79bff302d
Pavel Smirnov <pavel_smirnov1@epam.com>
2026-03-03 10:48:07 -0500

    Force SwiftShader

--- packages/miew/test/e2e/webdriver.cfg.js
+++ packages/miew/test/e2e/webdriver.cfg.js
@@ -1,6 +1,9 @@
 const chromeOptionsArguments = [
   '--headless', // headless mode (without browser window), it's more performance, but doesn't support some extensions
-  '--disable-gpu', // all computing in CPU, guaranteed the same image processing result on different machines
+  '--disable-gpu', // keep GPU acceleration disabled for e2e visual reproducibility
+  '--use-gl=angle', // route WebGL through ANGLE to make backend selection explicit
+  '--use-angle=swiftshader-webgl', // force software SwiftShader backend for deterministic rendering
+  '--enable-unsafe-swiftshader', // allow SwiftShader in recent Chromium versions
   '--log-level=1', // minimum log level: INFO = 0, WARNING = 1, LOG_ERROR = 2, LOG_FATAL = 3. level 1 - to not see info about not supporting some extensions
 ];
 
