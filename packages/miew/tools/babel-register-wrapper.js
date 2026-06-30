aa80efce1a64aa403926f60037d0589c44446452
Pavel Smirnov <pavel_smirnov1@epam.com>
2026-06-30 16:25:09 -0400

    Remove redundant fs.readFileSync in babel-register-wrapper

--- packages/miew/tools/babel-register-wrapper.js
+++ packages/miew/tools/babel-register-wrapper.js
@@ -1,11 +1,7 @@
-const fs = require('fs');
 const { addHook } = require('pirates');
 
 addHook(
-  (code, filename) => {
-    const content = fs.readFileSync(filename, 'utf8');
-    return `module.exports = ${JSON.stringify(content)};`;
-  },
+  (code) => `module.exports = ${JSON.stringify(code)};`,
   { exts: ['.vert', '.frag'] },
 );
 
